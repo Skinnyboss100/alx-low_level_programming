8785a3058dc01feb4df2c73f3e7fd8e156981c71
Skinnyboss100 <ademolaodusanya123@gmail.com>
2023-07-16 10:51:48 -0700

    Updated task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - program that prints using prinf function and returns 0
+ * main - Program that prints using the printf function and returns 0
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
