03c5442e0a446cae1db7759f768cd58d57d14b9d
Skinnyboss100 <ademolaodusanya123@gmail.com>
2023-07-16 09:21:31 -0700

    Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - A program that prints using puts
  *
- * Return : Always 0(success)
+ * Return : 0(success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle,");
 	return (0);
 }
