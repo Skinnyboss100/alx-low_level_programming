d585381e2e5f2fd75aa5c290b71c8c6f024ec84f
Skinnyboss100 <ademolaodusanya123@gmail.com>
2023-07-19 17:42:56 -0700

    First task done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,11 @@
 
 /**
  * main - program that prints _putchar
+ * _putchar - writes the character c to stand out
+ * @c: The character to print
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
+ * on error, -1 is returned, and errno is set
  */
 int _putchar(char c)
 {
