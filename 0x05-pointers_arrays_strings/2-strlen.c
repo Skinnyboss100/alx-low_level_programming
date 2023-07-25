abb9475d6a6e59ebad47703acd7a8a929d0f4b64
Skinnyboss100 <ademolaodusanya123@gmail.com>
2023-07-25 16:22:09 -0700

    Updated pointer task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
- * main - A function that returns the length of a string
+ * _strlen - returns the length of a string
  * @s: string
- * Return: Length of string
+ * Return: length
  */
-
 int _strlen(char *s)
 {
-    int measure = 0;
-    
-    while (*s != '\0' )
-    {
-        measure++;
-        s++;
-    }
-    return measure;
+	int measurement = 0;
+
+	while (*s != '\0')
+	{
+		measurement++;
+		s++;
+	}
+
+	return (measurement);
 }
+
